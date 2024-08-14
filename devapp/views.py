7402acd9384a732c271cc7167362ba3decc75d60
Vanga Ashok <ashokasmart01@gmail.com>
2024-08-14 15:07:40 +0530

    final commit

--- devapp/views.py
+++ devapp/views.py
@@ -6,6 +6,7 @@ import pandas as pd
 def upload_file(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
+        
         if form.is_valid():
             # Get the uploaded file
             excel_file = request.FILES['file']
@@ -18,7 +19,26 @@ def upload_file(request):
                 'total_rows': df.shape[0],
                 'total_columns': df.shape[1],
                 'column_names': df.columns.tolist(),
+                
             }
+            
+            # Select the necessary columns
+            df.columns = df.columns.str.replace(' ', '_')
+            selected_columns = df[['Cust_State', 'Cust_Pin', 'DPD']].dropna()
+
+            # Generate the table content
+            table_content = "\n".join(
+                f"<tr><td style='border:1px solid black;/n
+                border-collapse:collapse'>/n
+                {row['Cust_State']}</td>
+                <td style='border:1px solid black;/n
+                border-collapse:collapse'>
+                {row['Cust_Pin']}
+                <td style='border:1px solid black;border-collapse:collapse'>
+                {row['DPD']} </td></tr>"
+                for _, row in selected_columns.iterrows()
+            )
+            data = [row for _, row in selected_columns.iterrows()]
             
             # Prepare the email content
             email_subject = 'Python Assignment - Vanga Ashok'
@@ -29,6 +49,17 @@ def upload_file(request):
             Total Columns: {summary['total_columns']}
             Column Names: {', '.join(summary['column_names'])}
 
+            Detailed Table:
+
+            State\tCust Pin\tDPD
+            <table  style="border:1px solid black;border-collapse:collapse">
+            <tr>
+            <th style="border:1px solid black;border-collapse:collapse">State</th>
+            <th style="border:1px solid black;border-collapse:collapse">Postal Code</th>
+            <th style="border:1px solid black;border-collapse:collapse">DPD</th>
+            </tr>
+            {table_content}
+            </table>
             """
             
             # List of recipients
@@ -37,13 +68,14 @@ def upload_file(request):
             # Send the email
             send_mail(
                 email_subject,
-                email_body,
+                '',
                 'ashokavanga@gmail.com',  # Sender's email
                 recipient_list,
                 fail_silently=False,
+                html_message=email_body
             )
-            
-            return render(request, 'summary.html', {'summary': summary})
+            print(type(selected_columns.iterrows()))
+            return render(request, 'summary.html', {'summary': summary, 'data': data})
     else:
         form = UploadForm()
 
